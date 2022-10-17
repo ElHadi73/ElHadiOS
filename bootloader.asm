7d21b9d6c4be4f68a44d86222aa73bfff38a9a3b
gggg <gggg68131@gmail.com>
2022-10-17 12:07:52 -0400

    using a function to print

--- bootloader.asm
+++ bootloader.asm
@@ -1,35 +1,21 @@
+[org 0x7c00]
 
-mov ah, 0x0e
+mov bp, 0x7c00  			;this where the kernel get loaded to fist time and start running
+mov sp, bp				;put the stack pointer to 0x7c00
 
-mov al, 'H'
-int 0x10
-
-mov al, 'e'
-int 0x10
-
-mov al, 'l'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'o'
-int 0x10
-mov al, ' '
-int 0x10
-mov al, 'w'
-int 0x10
-mov al, 'o'
-int 0x10
-mov al, 'r'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'd'
-int 0x10
-mov al, '!'
-int 0x10
+call PrintString			;call the printstring function
 
 jmp $
 
-times 510-($-$$) db 0
+PrintString:
+mov bx, TestString			;make bx register to point to the first adress of the TestString variable
+	mov ah, 0x0e			;needed to print a text
+	mov al, [bx]			;take the content of the adress referd to by bx register
+	int 0x10
+	ret
 
+TestString:
+	db 'This is a test string',0
+
+times 510-($-$$) db 0
 dw 0xaa55
