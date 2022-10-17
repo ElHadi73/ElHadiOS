8689497e28f3292a41ba3a8d264bcbba489ac0fe
gggg <gggg68131@gmail.com>
2022-10-17 10:46:51 -0400

    print hello world when booting

--- bootloader.asm
+++ bootloader.asm
@@ -2,7 +2,30 @@
 mov ah, 0x0e
 
 mov al, 'H'
+int 0x10
 
+mov al, 'e'
+int 0x10
+
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'w'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'd'
+int 0x10
+mov al, '!'
 int 0x10
 
 jmp $
